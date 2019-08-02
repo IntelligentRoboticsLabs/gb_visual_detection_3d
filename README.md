74900fbaa2e12e13b7c010b9fbb136a3c79c3df9
Francisco Martin Rico <fmrico@gmail.com>
2019-08-02 07:50:23 +0200

    Remove BICA deps to better portability. Estetic changes.
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- README.md
+++ README.md
@@ -15,13 +15,10 @@ The first we have to know is that *darknet_ros_3d* package have dependencies as 
 * darknet_ros_3d_msgs
 * std_msgs
 * cv_bridge
-* bica
-* bica_msgs
 * roscpp
 * rospy
 
-**You can install Darknet Ros following** [this steps](https://github.com/leggedrobotics/darknet_ros) **and BICA following** [this steps](https://github.com/IntelligentRoboticsLabs/BICA).
-
+**You can install Darknet Ros following** [this steps](https://github.com/leggedrobotics/darknet_ros)
 ## Instalation
 
 You must to clone *darknet_ros_3d* into *src* folder located in your catkin workspace. Later, you have to compile it typing ``catkin_make``. If it doesn't work, it's possible that you must compile typing ``catkin_make -j1``.
@@ -34,18 +31,13 @@ For xtion RGBD camera, you have to install *openni2_launch* package and then, ty
 
 To run darknet ros, type ``roslaunch darknet_ros darknet_ros.launch``.
 
-Now, you can run *darknet_ros_3d* typing ``roslaunch darknet_ros_3d darknet_ros_3d.launch``. If you want to change default parameters like topics it subscribe, you can change it in the launch file. Default parameters are the following:
+Now, you can run *darknet_ros_3d* typing ``roslaunch darknet_ros_3d darknet_ros_3d.launch``.
 
-* **darknet_ros_topic:** topic where darknet ros publicates his bounding boxes. ``/darknet_ros/bounding_boxes``
 
-* **depth_image_topic:** topic where camera driver publicates the depth image. ``/camera/depth/image_raw``
+## Nodes
 
-* **dist_range_pixels:** max distance between nearest pixel and any other pixel of image to be countered.
+### darknet3d_node
 
-* **min_camera_dist:** minimum distance that camera is be able to detect.
+### Subscribed Topics
 
-This node is **inactive**, by default, because it's a BICA Component. **To active it** ```rosrun bica launcher Darknet_3d ```
-
-## Output
-
-The topic where darknet_ros_3d publicates his output is ``/darknet_ros_3d/bounding_boxes``.
+-
--- darknet_ros_3d/darknet_ros_3d/src/darknet_ros_3d/Darknet3D.cpp
+++ darknet_ros_3d/darknet_ros_3d/src/darknet_ros_3d/Darknet3D.cpp
@@ -1,3 +1,40 @@
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2019, Intelligent Robotics
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of Intelligent Robotics nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Francisco Martín fmrico@gmail.com */
+/* Author: Fernando González fergonzaramos@yahoo.es  */
+
 #include "darknet_ros_3d/Darknet3D.h"
 
 #include <ros/ros.h>
@@ -35,7 +72,6 @@ Darknet3D::initParams()
   pointcloud_topic_ = "/camera/depth_registered/points";
   working_frame_ = "/camera_link";
   mininum_detection_thereshold_ = 0.5f;
-  execution_frequency_ = 2.0;
 
   nh_.param("darknet_ros_topic", input_bbx_topic_, input_bbx_topic_);
   nh_.param("output_bbx3d_topic", output_bbx3d_topic_, output_bbx3d_topic_);
@@ -43,7 +79,6 @@ Darknet3D::initParams()
   nh_.param("working_frame", working_frame_, working_frame_);
   nh_.param("mininum_detection_thereshold", mininum_detection_thereshold_, mininum_detection_thereshold_);
   nh_.param("interested_classes", interested_classes_, interested_classes_);
-  nh_.param("execution_frequency", execution_frequency_, execution_frequency_);
 }
 
 void
